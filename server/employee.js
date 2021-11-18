25805bf734d311c23dca9b6fc5a55fe6e31c2d6f
Eidan <eidankhan.student.muet@gmail.com>
2021-11-18 10:43:04 +0500

    Created REST API for employees

---
+++ server/employee.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const router = express.Router();
+
+const generateUniqeId = () => {
+    return Math.floor(Math.random()*10000) 
+}
+
+let employees = [
+    {
+        "id": generateUniqeId(),
+        "name": "Eidan Khan",
+        "salary": 55000,
+        "position": "Software Design Engineer"
+    },
+    {
+        "id": generateUniqeId(),
+        "name": "Amir Hyder",
+        "salary": 65000,
+        "position": "Java App Consultant"
+    }
+];
+
+router.get('/employees', (req, res) => {
+    res.json(employees);
+})
+
+router.post("/employees", (req, res) => {
+    const {name, salary, position} = req.body;
+    const employee = {
+        "id": generateUniqeId(),
+        "name": name,
+        "salary": salary,
+        "position": position,
+    }
+    employees.push(employee);
+    res.json(employee);
+})
+
+router.delete("/employees/:id", (req, res) => {
+    employees = employees.filter( (item) => { return item.id != req.params.id});
+    res.send("Deleted employee with id:"+req.params.id);
+})
+
+router.put("/employees/:id", (req, res) => {
+    const {name, salary, position} = req.body;
+    const id = req.params.id;
+    let employee = employees.find(obj => obj.id == id);
+    if(employee){
+        employee.name = name;
+        employee.salary = salary;
+        employee.position = position;
+    }
+    
+    res.send(employee)
+    
+})
+
+
+module.exports = router;
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const employeeRoutes = require("./employee")
+// const cors = require('cors');
+
+const app = express();
+const port = "2900";
+
+// app.use(cors());
+app.use(express.json());
+app.listen(port, () => { console.log(`App started at port: ${port}`)});
+
+// Printing all the available routes for book
+//console.log('Routes:'+bookRoutes.stack.filter(r=> r.route).map(r=>r.route.path));
+
+app.use("/api", employeeRoutes)
